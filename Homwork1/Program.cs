95b9b8ed4b29a4b9346aca247604be8e2dd3c7af
Sona <sona_p86@list.ru>
2022-11-19 10:26:17 +0300

    homwork1-2

---
+++ Homwork1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 2 написать программу, которая на вход принимает два числа и выдает,какое число больше а какое меньше//
+/*Console.WriteLine("Input number 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine($"{num1} > {num2}");
+}
+else
+{
+Console.WriteLine($"{num2} > {num1}");
+}*/
+
+//Задача 4 максимальное из 3 чисел//
+Console.WriteLine("Input number 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+Console.WriteLine($"{max}");
