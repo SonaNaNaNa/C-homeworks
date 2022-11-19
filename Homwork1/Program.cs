9b2ec0129a197e843ec3fcf5986c9a2a7cc86952
Sona <sona_p86@list.ru>
2022-11-19 12:07:32 +0300

    задания3-4

--- Homwork1/Program.cs
+++ Homwork1/Program.cs
@@ -13,13 +13,40 @@ Console.WriteLine($"{num2} > {num1}");
 }*/
 
 //Задача 4 максимальное из 3 чисел//
-Console.WriteLine("Input number 1");
+/*Console.WriteLine("Input number 1");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input number 2");
 int num2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input number ");
 int num3 = Convert.ToInt32(Console.ReadLine());
-max = num1;
+int max = num1;
 if (num2 > max) max = num2;
 if (num3 > max) max = num3;
-Console.WriteLine($"{max}");
+Console.WriteLine($"Maximum is {max}");*/
+
+// Задача 6 определить является ли число четным//
+
+/*Console.WriteLine("Input number");
+int numA = Convert.ToInt32(Console.ReadLine());
+int numB = numA % 2;
+if (numB == 0)
+{
+    Console.WriteLine($"{numA} четное число");
+}
+    else
+{
+    Console.WriteLine($"{numA} нечетное число");
+}*/
+
+//Задача8 четные числа от 1 до N//
+
+
+/*Console.WriteLine("Input numberN");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (count <= num1)
+{
+    Console.WriteLine(count);
+    count++;
+    count++;
+}*/
